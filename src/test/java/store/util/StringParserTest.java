64cee723f76a49351b0a1b0feeb3ef30eaa00532
openmpy <suhwan@kakao.com>
2024-11-09 13:03:10 +0900

    test: StringParser 클래스 테스트 케이스 추가
    - 문자열을 입력 받아 Promotion 클래스로 변환
    - 문자열을 입력 받아 프로모션이 존재하는 Product 클래스로 변환

--- src/test/java/store/util/StringParserTest.java
+++ src/test/java/store/util/StringParserTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.domain.Product;
 import store.domain.Promotion;
 
 class StringParserTest {
@@ -24,5 +25,39 @@ class StringParserTest {
         assertThat(promotion.getGetAmount()).isEqualTo(1);
         assertThat(promotion.getStartDate()).isEqualTo(LocalDate.of(2024, 1, 1));
         assertThat(promotion.getEndDate()).isEqualTo(LocalDate.of(2024, 12, 31));
+    }
+
+    @DisplayName("[통과] 문자열을 입력 받아 프로모션이 존재하는 Product 클래스로 변환")
+    @Test
+    void string_parser_test_02() {
+        // given
+        final String string = "콜라,1000,10,탄산2+1";
+
+        // when
+        final Product product = StringParser.parseProduct(string);
+
+        // then
+        assertThat(product.getName()).isEqualTo("콜라");
+        assertThat(product.getPrice()).isEqualTo(1000);
+        assertThat(product.getDefaultQuantity()).isZero();
+        assertThat(product.getPromotionQuantity()).isEqualTo(10);
+        assertThat(product.getPromotionName()).isEqualTo("탄산2+1");
+    }
+
+    @DisplayName("[통과] 문자열을 입력 받아 프로모션이 존재하지 않는 Product 클래스로 변환")
+    @Test
+    void string_parser_test_03() {
+        // given
+        final String string = "콜라,1000,10,null";
+
+        // when
+        final Product product = StringParser.parseProduct(string);
+
+        // then
+        assertThat(product.getName()).isEqualTo("콜라");
+        assertThat(product.getPrice()).isEqualTo(1000);
+        assertThat(product.getDefaultQuantity()).isEqualTo(10);
+        assertThat(product.getPromotionQuantity()).isZero();
+        assertThat(product.getPromotionName()).isNull();
     }
 }
