7c3477977382e42cd7a21d70add1de16a44917d4
openmpy <suhwan@kakao.com>
2024-11-09 14:25:19 +0900

    feat: 상품 목록에서 상품명이 중복되면 합치는 기능 추가

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,13 +1,16 @@
 package store.domain;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Products {
 
     private final List<Product> products;
 
     public Products(final List<Product> products) {
-        this.products = products;
+        this.products = updateProducts(products);
     }
 
     public Product findProductsByName(final String productName) {
@@ -15,6 +18,32 @@ public class Products {
                 .filter(product -> product.getName().equals(productName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    private List<Product> updateProducts(final List<Product> products) {
+        final Map<String, Product> productMap = new LinkedHashMap<>();
+        for (final Product product : products) {
+            if (!productMap.containsKey(product.getName())) {
+                productMap.put(product.getName(), product);
+                continue;
+            }
+            final Product existingProduct = productMap.get(product.getName());
+            final Product newProduct = updateProduct(product, existingProduct);
+            productMap.put(product.getName(), newProduct);
+        }
+        return new ArrayList<>(productMap.values());
+    }
+
+    private static Product updateProduct(final Product product, final Product existingProduct) {
+        final int newDefaultQuantity = product.getDefaultQuantity() + existingProduct.getDefaultQuantity();
+        final int newPromotionQuantity = product.getPromotionQuantity() + existingProduct.getPromotionQuantity();
+        String newPromotionName = existingProduct.getPromotionName();
+
+        if (product.getPromotionName() != null) {
+            newPromotionName = product.getPromotionName();
+        }
+        return new Product(product.getName(), product.getPrice(), newDefaultQuantity, newPromotionQuantity,
+                newPromotionName);
     }
 
     public List<Product> getProducts() {
