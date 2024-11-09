90693b3fd545f3fb63574cb4c719c1ba2e11e412
openmpy <suhwan@kakao.com>
2024-11-09 12:57:06 +0900

    test: Product 클래스 테스트 케이스 추가
    - 프로모션이 존재하는 Product 클래스 생성
    - 프로모션이 존재하지 않는 Product 클래스 생성

---
+++ src/test/java/store/domain/ProductTest.java
@@ -0,0 +1,37 @@
+package store.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+    @DisplayName("[통과] 프로모션이 존재하는 Product 클래스 생성")
+    @Test
+    void product_test_01() {
+        // when
+        final Product product = new Product("콜라", 1000, 10, "2+1");
+
+        // then
+        assertThat(product.getName()).isEqualTo("콜라");
+        assertThat(product.getPrice()).isEqualTo(1000);
+        assertThat(product.getDefaultQuantity()).isZero();
+        assertThat(product.getPromotionQuantity()).isEqualTo(10);
+        assertThat(product.getPromotionName()).isEqualTo("2+1");
+    }
+
+    @DisplayName("[통과] 프로모션이 존재하지 않는 Product 클래스 생성")
+    @Test
+    void product_test_02() {
+        // when
+        final Product product = new Product("콜라", 1000, 10, "null");
+
+        // then
+        assertThat(product.getName()).isEqualTo("콜라");
+        assertThat(product.getPrice()).isEqualTo(1000);
+        assertThat(product.getDefaultQuantity()).isEqualTo(10);
+        assertThat(product.getPromotionQuantity()).isZero();
+        assertThat(product.getPromotionName()).isNull();
+    }
+}
