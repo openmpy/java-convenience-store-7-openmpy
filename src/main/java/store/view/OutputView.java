8bfc73be28efb662aa419a8b33bae154c0e04eca
openmpy <suhwan@kakao.com>
2024-11-09 13:21:49 +0900

    feat: 환영인사, 상품 정보를 안내하는 메세지 출력 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,10 +1,26 @@
 package store.view;
 
+import java.util.List;
+import store.domain.Product;
+
 public class OutputView {
 
     public static void printWelcomeMessage() {
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.");
+        System.out.println();
+    }
+
+    public static void printProductsInfo(final List<Product> products) {
+        for (final Product product : products) {
+            final String formatted = String.format("- %s %s원 %s %s",
+                    product.getName(),
+                    product.getFormattedPrice(),
+                    product.getQuantityState(product.getCustomQuantity()),
+                    product.getCustomPromotionName()
+            );
+            System.out.println(formatted);
+        }
         System.out.println();
     }
 }
