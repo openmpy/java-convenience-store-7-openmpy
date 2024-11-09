83fc51a473ffe32407d7a51487dcf428e9a1ab66
openmpy <suhwan@kakao.com>
2024-11-09 18:27:37 +0900

    feat: 프로모션 적용 여부를 입력 받는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,4 +8,26 @@ public class InputView {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
+
+    public static String readGetBonusProduct(final String productName, final int bonusQuantity) {
+        System.out.println();
+        final String formatted =
+                String.format("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", productName, bonusQuantity);
+        System.out.println(formatted);
+        return Console.readLine();
+    }
+
+    public static String readNotApplyBonusProduct(final String productName, final int bonusQuantity) {
+        System.out.println();
+        final String formatted =
+                String.format("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", productName, bonusQuantity);
+        System.out.println(formatted);
+        return Console.readLine();
+    }
+
+    public static String readMembershipDiscount() {
+        System.out.println();
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
 }
