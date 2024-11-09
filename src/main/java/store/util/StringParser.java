fbc3c6fa67f1181bebc91d09dc1242c4531f8a63
openmpy <suhwan@kakao.com>
2024-11-09 13:02:36 +0900

    feat: 문자열을 Product 클래스로 변환하는 기능 추가

--- src/main/java/store/util/StringParser.java
+++ src/main/java/store/util/StringParser.java
@@ -2,6 +2,7 @@ package store.util;
 
 import java.time.LocalDate;
 import java.util.List;
+import store.domain.Product;
 import store.domain.Promotion;
 
 public class StringParser {
@@ -14,5 +15,14 @@ public class StringParser {
         final LocalDate startDate = LocalDate.parse(items.get(3));
         final LocalDate endDate = LocalDate.parse(items.get(4));
         return new Promotion(promotionName, buyAmount, getAmount, startDate, endDate);
+    }
+
+    public static Product parseProduct(final String string) {
+        final List<String> items = StringSplitter.splitComma(string);
+        final String productName = items.get(0);
+        final int price = Integer.parseInt(items.get(1));
+        final int quantity = Integer.parseInt(items.get(2));
+        final String promotionName = items.get(3);
+        return new Product(productName, price, quantity, promotionName);
     }
 }
