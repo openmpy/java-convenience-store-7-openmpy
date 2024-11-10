a5574081f2df2e76138a9f728f286e7d790abeb6
openmpy <suhwan@kakao.com>
2024-11-10 15:59:39 +0900

    test: Promotion 클래스 테스트 케이스 추가
    - 프로모션 행사 기간에 해당하는 경우 true 반환
    - 프로모션 행사 기간에 해당하지 않는 경우 false 반환

--- src/test/java/store/domain/PromotionTest.java
+++ src/test/java/store/domain/PromotionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -47,5 +48,35 @@ class PromotionTest {
 
         // then
         assertThat(bonusProduct).isEqualTo(expected);
+    }
+
+    @DisplayName("[통과] 프로모션 행사 기간에 해당하는 경우 true 반환")
+    @Test
+    void promotion_test_03() {
+        // given
+        final LocalDate startDate = LocalDate.of(2024, 1, 1);
+        final LocalDate endDate = LocalDate.of(2024, 12, 31);
+        final Promotion promotion = new Promotion("2+1", 2, 1, startDate, endDate);
+
+        // when
+        final boolean isPromotionValid = promotion.isApplyDate(LocalDate.of(2024, 6, 1));
+
+        // then
+        assertThat(isPromotionValid).isTrue();
+    }
+
+    @DisplayName("[통과] 프로모션 행사 기간에 해당하지 않는 경우 false 반환")
+    @Test
+    void promotion_test_04() {
+        // given
+        final LocalDate startDate = LocalDate.of(2024, 1, 1);
+        final LocalDate endDate = LocalDate.of(2024, 12, 31);
+        final Promotion promotion = new Promotion("2+1", 2, 1, startDate, endDate);
+
+        // when
+        final boolean isPromotionValid = promotion.isApplyDate(LocalDate.of(2023, 6, 1));
+
+        // then
+        assertThat(isPromotionValid).isFalse();
     }
 }
