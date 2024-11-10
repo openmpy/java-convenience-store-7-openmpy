9649065468be5ae857e8c677783197dd655aafdf
openmpy <suhwan@kakao.com>
2024-11-10 16:15:02 +0900

    refactor: ProductService 클래스 메서드 분리

--- src/main/java/store/service/ProductService.java
+++ src/main/java/store/service/ProductService.java
@@ -1,10 +1,10 @@
 package store.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import store.dto.ProductPurchaseDto;
+import store.exception.InvalidInputException;
 import store.util.StringSplitter;
 
 public class ProductService {
@@ -12,21 +12,23 @@ public class ProductService {
     private static final String SQUARE_BRACKETS_REGEX = "^\\[([^]-]+)-([^]]+)]$";
 
     public List<ProductPurchaseDto> addProductPurchase(final String info) {
-        final List<ProductPurchaseDto> productPurchaseDtos = new ArrayList<>();
-        final Pattern pattern = Pattern.compile(SQUARE_BRACKETS_REGEX);
+        return StringSplitter.splitComma(info).stream()
+                .map(this::extractProductPurchase)
+                .toList();
+    }
 
-        StringSplitter.splitComma(info).forEach(it -> {
-            final Matcher matcher = pattern.matcher(it);
-            if (!matcher.matches()) {
-                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
-            }
+    private ProductPurchaseDto extractProductPurchase(final String input) {
+        final Matcher matcher = Pattern.compile(SQUARE_BRACKETS_REGEX).matcher(input);
+        validateInput(matcher);
 
-            final String productName = matcher.group(1).strip();
-            final int purchaseQuantity = Integer.parseInt(matcher.group(2).strip());
+        final String productName = matcher.group(1).strip();
+        final int purchaseQuantity = Integer.parseInt(matcher.group(2).strip());
+        return new ProductPurchaseDto(productName, purchaseQuantity);
+    }
 
-            final ProductPurchaseDto productPurchaseDto = new ProductPurchaseDto(productName, purchaseQuantity);
-            productPurchaseDtos.add(productPurchaseDto);
-        });
-        return productPurchaseDtos;
+    private void validateInput(final Matcher matcher) {
+        if (!matcher.matches()) {
+            throw new InvalidInputException();
+        }
     }
 }
