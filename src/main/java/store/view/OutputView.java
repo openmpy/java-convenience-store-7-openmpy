e830802cba0677f07b6954746ae74777222811d2
openmpy <suhwan@kakao.com>
2024-11-11 13:14:38 +0900

    refactor: 영수증 출력 문자열 정렬하기

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -27,11 +27,11 @@ public class OutputView {
     }
 
     public static void printProductPurchaseInfo(final Order order) {
-        System.out.println("상품명 수량 금액");
+        System.out.printf("%-10s %-5s %-5s %n", "상품명", "수량", "금액");
         for (final Product product : order.getOrderItems().keySet()) {
             final Integer buyQuantity = order.getOrderItems().get(product);
-            final String formatted = String.format("%s %d %s", product.getName(), buyQuantity,
-                    NumberFormatter.apply(buyQuantity * product.getPrice()));
+            final String formatted = String.format("%-10s %-5d %-5s",
+                    product.getName(), buyQuantity, NumberFormatter.apply(buyQuantity * product.getPrice()));
             System.out.println(formatted);
         }
     }
@@ -42,7 +42,7 @@ public class OutputView {
         }
         System.out.println("===========증\t정=============");
         for (final Product product : order.getBonusItems().keySet()) {
-            final String formatted = String.format("%s %d", product.getName(), order.getBonusItems().get(product));
+            final String formatted = String.format("%-10s %-5d", product.getName(), order.getBonusItems().get(product));
             System.out.println(formatted);
         }
     }
@@ -80,20 +80,22 @@ public class OutputView {
         final int totalPurchaseQuantity = order.getTotalPurchaseQuantity();
         final int totalPurchasePrice = order.getTotalPurchasePrice();
         final String totalPriceInfo =
-                String.format("총구매액 %d %s", totalPurchaseQuantity, NumberFormatter.apply(totalPurchasePrice));
+                String.format("%-10s %-5d %-5s",
+                        "총구매액", totalPurchaseQuantity, NumberFormatter.apply(totalPurchasePrice));
         System.out.println(totalPriceInfo);
     }
 
     private static void printReceiptBonusPrice(final Order order) {
         final int totalBonusProductPrice = order.getTotalBonusProductPrice();
-        final String totalBonusPriceInfo = String.format("행사할인 -%s", NumberFormatter.apply(totalBonusProductPrice));
+        final String totalBonusPriceInfo =
+                String.format("%-10s -%-5s", "행사할인", NumberFormatter.apply(totalBonusProductPrice));
         System.out.println(totalBonusPriceInfo);
     }
 
     private static void printReceiptMembershipDiscount(final Order order) {
         final int totalMembershipPrice = order.getTotalMembershipPrice();
         final String totalMembershipDiscountInfo =
-                String.format("멤버십할인 -%s", NumberFormatter.apply(totalMembershipPrice));
+                String.format("%-10s -%-5s", "멤버십할인", NumberFormatter.apply(totalMembershipPrice));
         System.out.println(totalMembershipDiscountInfo);
     }
 
@@ -102,10 +104,11 @@ public class OutputView {
         final int totalBonusProductPrice = order.getTotalBonusProductPrice();
         final int totalMembershipPrice = order.getTotalMembershipPrice();
 
-        final String sendMoneyInfo =
-                String.format("내실돈 %s", NumberFormatter.apply(
-                        totalPurchasePrice - totalBonusProductPrice - totalMembershipPrice)
-                );
+        final String sendMoneyInfo = String.format(
+                "%-10s %-5s",
+                "내실돈",
+                NumberFormatter.apply(totalPurchasePrice - totalBonusProductPrice - totalMembershipPrice)
+        );
 
         System.out.println(sendMoneyInfo);
     }
